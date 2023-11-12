7953744c555a7ebe42ff3f79f4c164220b7d4b5e
Paul M <arquolo@gmail.com>
2023-11-12 22:20:13 +0500

    FEAT roc acc dice

--- src/rajo/metrics/roc.py
+++ src/rajo/metrics/roc.py
@@ -1,6 +1,5 @@
 __all__ = [
-    'Confusion', 'auc', 't_balance', 't_dice', 't_otsu', 't_youden_j',
-    'youden_j'
+    'Confusion', 'acc', 'auc', 'dice', 't_balance', 't_otsu', 'youden_j'
 ]
 
 from collections.abc import Callable
@@ -70,7 +69,7 @@ def t_sup(mat: Tensor) -> Tensor:
     return idx.float() / (mat.shape[0] - 1)
 
 
-def t_acc(mat: Tensor) -> Tensor:
+def acc(mat: Tensor) -> dict[str, Tensor]:
     """
     Probability value where `TP + TN = max`.
     Tx2x2 tensor to scalar.
@@ -78,40 +77,28 @@ def t_acc(mat: Tensor) -> Tensor:
     assert mat.shape[1:] == (2, 2)
     acc = mat.diagonal(dim1=1, dim2=2).sum(1)
     acc = acc / mat.sum((1, 2)).clamp_min_(_EPS)
-    idx = acc.argmax()
-    return idx.float() / (mat.shape[0] - 1)
+    return _max_t(acc)
 
 
-def youden_j(mat: Tensor) -> Tensor:
+def youden_j(mat: Tensor) -> dict[str, Tensor]:
     """
     Max of Youden's J statistic (i.e. informedness).
     Computed as `max(tpr - fpr)`, or `max(2 * balanced accuracy - 1)`.
     Tx2x2 tensor to scalar.
     """
     fpr, tpr = fpr_tpr(mat)
-    return (tpr - fpr).amax()
+    return _max_t(tpr - fpr)
 
 
-def t_youden_j(mat: Tensor) -> Tensor:
+def dice(mat: Tensor) -> dict[str, Tensor]:
     """
-    Probability value where is max of Youden's J statistic.
-    Tx2x2 tensor to scalar.
-    """
-    fpr, tpr = fpr_tpr(mat)
-    idx = (tpr - fpr).argmax()
-    return idx.float() / (mat.shape[0] - 1)
-
-
-def t_dice(mat: Tensor) -> Tensor:
-    """
-    Probability threshold for Dice score maximum.
+    Dice score maximum.
     Tx2x2 tensor to scalar.
     """
     tp = mat[:, 1, 1]
     fp_tp = mat[:, :, 1].sum(1)
     fn_tp = mat[:, 1, :].sum(1)
-    idx = (tp / (fp_tp + fn_tp).clamp_min_(_EPS)).argmax()
-    return idx.float() / (mat.shape[0] - 1)
+    return _max_t(2 * tp / (fp_tp + fn_tp).clamp_min_(_EPS))
 
 
 def support(mat: Tensor) -> Tensor:
@@ -137,3 +124,8 @@ def t_otsu(mat: Tensor) -> Tensor:
     t = (x_cdf.square().div_(cdf.clamp_min(_EPS)) +
          (p_mean - x_cdf).square_().div_((1 - cdf).clamp_min_(_EPS))).argmax()
     return t / (n - 1)
+
+
+def _max_t(xs: Tensor) -> dict[str, Tensor]:
+    value, idx = xs.max(0)
+    return {'': value, 't': idx.float() / (xs.shape[0] - 1)}
